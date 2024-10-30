e5f7e528aa14f43e642e90442d64ee8804fe6c52
xuhj <2416687496@qq.com>
2024-10-30 20:40:00 +0800

    non

--- onix/src/boot.asm
+++ onix/src/boot.asm
@@ -1,29 +1,29 @@
 [org 0x7c00]
 
-
 ;设置屏幕模式为文本模式
 mov ax, 3
 int 0x10
 
+;初始化段寄存器，段寄存器只能使用ax,bx这类的通用寄存器复制，没有给段寄存器直接赋值的电路
 mov ax, 0
 mov ds, ax
 mov es, ax
 mov ss, ax
-mov sp, 0x7c00
+mov sp, 0x7c00  ; 设置栈指针，栈向地址低的地方扩展
 
-mov si, booting
+mov si, booting  ; booting经编译后是一个地址，将这个地址赋值给源地址基址寄存器
 call print
 
-mov edi, 0x1000
+mov edi, 0x1000  ;将磁盘数据读取到0x1000这个位置
 mov ecx, 2
 mov bl, 4
 
 call read_disk
 
-cmp word [0x1000], 0x55aa
+cmp word[0x1000], 0x55aa
 jnz error
 
-jmp 0:0x1002
+jmp 0:0x1008 ;将loader读取到0x1000这个位置，起始8个字节以占用
 
 jmp $
 
@@ -58,14 +58,14 @@ read_disk:
 	mov al, 0x20
 	out dx, al
 
-	xor ecx, ecx
-	mov cl, bl
+	xor ecx, ecx  ; 异或，ecx清零
+	mov cl, bl    ; 读取扇区的数量
 
 	.read:
-		push cx
+		push cx   ; [call .waits] 和 [call .reads]是读取一个扇区
 		call .waits
 		call .reads
-		pop cx
+		pop cx    ; 这儿弹出cx使得之后的loop将其值减一
 		loop .read
 
 	ret	
@@ -80,7 +80,7 @@ read_disk:
 			and al, 0b1000_1000
 			cmp al, 0b0000_1000
 			jnz .check
-		ret
+	ret
 
 	.reads:
 		mov dx, 0x1f0
@@ -93,7 +93,7 @@ read_disk:
 			mov [edi], ax
 			add edi, 2
 			loop .readw
-		ret
+	ret
 
 print:
 	mov ah, 0x0e
@@ -113,7 +113,7 @@ booting:
 error:
 	mov si, .msg
 	call print
-	hlt
+	hlt ; 让cpu停止
 	jmp $
 	.msg db "Booting Error!!", 10, 13, 0
 
