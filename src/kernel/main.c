8af2df02452e8130d55796bf16a8c7d056bd0cff
xuhj <2416687496@qq.com>
2024-12-02 06:29:33 +0000

    内存映射原理

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,9 @@
+
+#include <onix/debug.h>
+#define LOGK(fmt, args...) DEBUGK(fmt, ##args)
+
 extern void memory_map_init();
+extern void mapping_init();
 extern void interrupt_init();
 extern void clock_init();
 extern void hang();
@@ -10,13 +15,18 @@ extern void memory_test();
 void kernel_init()
 {
 	memory_map_init();
+	mapping_init();
 	interrupt_init();
 
-	clock_init();
+        //clock_init();
 	//time_init();	
 	//rtc_init();	
-	memory_test();
     	
+    	BMB;
+
+	char *ptr = (char *)(0x100000 * 20);
+	ptr[0] = 'a';
+
 	asm volatile("sti");
-    	hang();
+	hang();
 }
